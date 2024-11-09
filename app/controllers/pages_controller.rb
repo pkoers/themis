4acaa6dc079a8d3876e68dc4e0a8213c6382e26e
Patrick Koers <pkoers75@gmail.com>
2024-11-09 20:35:04 +0100

    open JSON file and convert to hash

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
+    puts "reading JSON File"
+    file = File.read('./storage/history.json')
+    puts "Converting JSON File"
+    data_hash = JSON.parse(file)
   end
 end
