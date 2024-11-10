1af5290bbd68571d014544c2ed06e88a3e07a733
Patrick Koers <pkoers75@gmail.com>
2024-11-10 22:29:42 +0100

    def to create an array that contains all the registration changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,10 @@ class PagesController < ApplicationController
     # Converting the JSON file
     puts "Converting JSON File"
     data_hash = JSON.parse(file)
+    data = JSON.parse(file, symbolize_names: true)
     # Grabbing the Flight Details
     @flight_details = FlightDetails(data_hash)
+    puts RegoChanges(data)
   end
 
   def FlightDetails(data_hash)
@@ -17,4 +19,23 @@ class PagesController < ApplicationController
     origin = data_hash['criteria']['boardpoint']
     return "#{carrier}#{flight}/#{date} Origin:#{origin}"
   end
+
+  def RegoChanges(data_hash)
+    results = []
+
+    data_hash[:hist].each do |entry|
+      if entry[:app] == "DCS"
+        entry[:data].each do |data_item|
+          if data_item[:type] == "Free Text" && data_item[:value].is_a?(Array)
+            data_item[:value].each do |value|
+              results << value if value.start_with?("Registration changed")
+            end
+          end
+        end
+      end
+    end
+    # return the array results that contain all the registration changes
+    return results
+  end
+
 end
